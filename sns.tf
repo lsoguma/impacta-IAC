acb0b4448ccb4e55c1b5c1e0da807f4e1a93b476
lsoguma <124442926+lsoguma@users.noreply.github.com>
2023-03-27 21:22:47 -0300

    Add SQS policy

--- sns.tf
+++ sns.tf
@@ -11,3 +11,29 @@ resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
   protocol  = "sqs"
   endpoint  = aws_sqs_queue.sqs-class.arn
 }
+
+data "aws_iam_policy_document" "test" {
+  statement {
+    sid    = "First"
+    effect = "Allow"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions   = ["sqs:SendMessage"]
+    resources = [aws_sqs_queue.sqs-class.arn]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_sns_topic.example.arn]
+    }
+  }
+}
+
+resource "aws_sqs_queue_policy" "test" {
+  queue_url = aws_sqs_queue.sqs-class.id
+  policy    = data.aws_iam_policy_document.test.json
+}
